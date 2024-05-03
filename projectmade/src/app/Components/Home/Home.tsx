c516b767ddfdf98240f7b3cdb8a1b1ffa3757ac8
Dario D'Alessandro <dario.dalessandro@marconirovereto.it>
2024-05-03 10:34:24 +0200

    fue caroselli

--- projectmade/src/app/Components/Home/Home.tsx
+++ projectmade/src/app/Components/Home/Home.tsx
@@ -3,9 +3,11 @@ import Image from "next/image";
 import { useState } from "react";
 
 export default function Home() {
-    let [currentImageIndex, setCurrentImageIndex] = useState(0);
-    const foto = ['foto1.jpg', 'foto2.jpg', 'foto3.jpg'];
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
+    const [currentImageIndexSecond, setCurrentImageIndexSecond] = useState(0);
+    const foto = ['foto1.jpg', 'foto2.jpg', 'foto3.jpg', 'foto4.jpg'];
     const secondFoto = ['foto1.jpg', 'foto2.jpg', 'foto3.jpg', 'foto4.jpg'];
+
     const handleNextImage = () => {
         setCurrentImageIndex((currentImageIndex + 1) % foto.length);
     };
@@ -14,40 +16,30 @@ export default function Home() {
         setCurrentImageIndex((currentImageIndex - 1 + foto.length) % foto.length);
     };
 
+    const handleNextImageSecond = () => {
+        setCurrentImageIndexSecond((currentImageIndexSecond + 1) % secondFoto.length);
+    };
+
+    const handlePreviousImageSecond = () => {
+        setCurrentImageIndexSecond((currentImageIndexSecond - 1 + secondFoto.length) % secondFoto.length);
+    };
+
     const handleImageClick = (index: number) => {
         setCurrentImageIndex(index);
     };
 
-    const handleNextImageSecond = () => {
-        setCurrentImageIndex((currentImageIndex + 1) % secondFoto.length);
-    };
-
-    const handlePreviousImageSecond = () => {
-        setCurrentImageIndex((currentImageIndex - 1 + secondFoto.length) % secondFoto.length);
-    };
-
     const handleImageClickSecond = (index: number) => {
-        setCurrentImageIndex(index);
+        setCurrentImageIndexSecond(index);
     };
 
     return (
         <div>
             <button onClick={handlePreviousImage}>Previous</button>
-            <Image src={`/${foto[currentImageIndex]}`} alt="Current Image" width="500" height="300" />
+            <Image src={`/${foto[currentImageIndex]}`} alt="Current Image" width="500" height="300" onClick={() => handleImageClick(currentImageIndex)} />
             <button onClick={handleNextImage}>Next</button>
-
-            <div>
-                {secondFoto.map((image, index) => (
-                    <img
-                        key={index}
-                        src={`/${image}`}
-                        alt={`Image ${index}`}
-                        width="100"
-                        height="100"
-                        onClick={() => handleImageClickSecond(index)}
-                    />
-                ))}
-            </div>
+            <button onClick={handlePreviousImageSecond}>Previous</button>
+            <Image src={`/${secondFoto[currentImageIndexSecond]}`} alt="Current Image" width="500" height="300" onClick={() => handleImageClickSecond(currentImageIndexSecond)} />
+            <button onClick={handleNextImageSecond}>Next</button>
         </div>
     );
 }
