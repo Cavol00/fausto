2fc532652ea3c0ace6977f8974b0ed5631cf6d59
Dario D'Alessandro <dario.dalessandro@marconirovereto.it>
2024-05-03 11:42:40 +0200

    Caroselli aggiornati

--- projectmade/src/app/Components/Home/Home.tsx
+++ projectmade/src/app/Components/Home/Home.tsx
@@ -1,12 +1,12 @@
-"use client";
+"use client"
 import Image from "next/image";
 import { useState } from "react";
 
 export default function Home() {
     const [currentImageIndex, setCurrentImageIndex] = useState(0);
     const [currentImageIndexSecond, setCurrentImageIndexSecond] = useState(0);
-    const foto = ['foto1.png', 'foto2.png', 'foto3.png', 'foto4.png','foto5.png', 'foto6.png', 'foto7.png', 'foto8.png', 'foto9.png', 'foto10.png', 'foto11.png', 'foto12.png'];
-    const secondFoto = ['foto1.png', 'foto2.png', 'foto3.png', 'foto4.png','foto5.png', 'foto6.png', 'foto7.png', 'foto8.png', 'foto9.png', 'foto10.png', 'foto11.png', 'foto12.png'];
+    const foto = ['foto1.png', 'foto2.png', 'foto3.png', 'foto4.png', 'foto5.png', 'foto6.png', 'foto7.png', 'foto8.png', 'foto9.png', 'foto10.png', 'foto11.png', 'foto12.png'];
+    const secondFoto = ['foto1.png', 'foto2.png', 'foto3.png', 'foto4.png', 'foto5.png', 'foto6.png', 'foto7.png', 'foto8.png', 'foto9.png', 'foto10.png', 'foto11.png', 'foto12.png'];
 
     const handleNextImage = () => {
         setCurrentImageIndex((currentImageIndex + 1) % foto.length);
@@ -26,16 +26,13 @@ export default function Home() {
 
     const handleImageClick = (index: number) => {
         setCurrentImageIndex(index);
+        setCurrentImageIndexSecond(index); // Synchronize the second index with the first index
     };
 
-    const handleImageClickFirst = (index: number) => {
-        setCurrentImageIndex(index);
-        setCurrentImageIndexSecond(index);
-    }
     const handleImageClickSecond = (index: number) => {
         setCurrentImageIndexSecond(index);
+        setCurrentImageIndex(index); // Synchronize the first index with the second index
     };
-    
 
     return (
         <div>
