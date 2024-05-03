c46bb5e7f9728acf4809ed7ffb02b1e0733fef45
jiahong <jiahong.zhou@marconirovereto.it>
2024-05-03 10:10:23 +0200

    yes

--- projectmade/src/app/Components/Mappa/Mappa.tsx
+++ projectmade/src/app/Components/Mappa/Mappa.tsx
@@ -4,6 +4,7 @@ import "leaflet/dist/leaflet.css";
 import "leaflet-defaulticon-compatibility";
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
 
+
 export default function Mappa(props: any) {
   return (
     <MapContainer center={[45.887384120868745, 11.033286614780325]} zoom={13}  style={{ height: '100vh', width: '100wh'} } zoomControl = {false}>
