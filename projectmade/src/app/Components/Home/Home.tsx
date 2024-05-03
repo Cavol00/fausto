251a5e8f43c2690a6d86a1fb38b9cd72e1782ce0
Dario D'Alessandro <dario.dalessandro@marconirovereto.it>
2024-05-03 09:28:28 +0200

    Ciao

--- projectmade/src/app/Components/Home/Home.tsx
+++ projectmade/src/app/Components/Home/Home.tsx
@@ -1,21 +1,26 @@
 "use client";
 import Image from "next/image";
-export default function Home() {
-    const foto = ["foto1.jpg", "foto2.jpg", "foto3.jpg", "foto4.jpg"];
-    let currentImageIndex = 0;
+import foto1 from "./image/foto1.jpg";
+import foto2 from "./image/foto2.jpg";
+import foto3 from "./image/foto3.jpg";
+import foto4 from "./image/foto4.jpg";
+import { useState } from "react";
 
+export default function Home() {
+    let [currentImageIndex,setCurrentImageIndex] = useState(0);
+    const foto = [foto1, foto2, foto3, foto4];
     const handleNextImage = () => {
-        currentImageIndex = (currentImageIndex + 1) % foto.length;
+        setCurrentImageIndex((currentImageIndex + 1) % foto.length);
     };
 
     const handlePreviousImage = () => {
-        currentImageIndex = (currentImageIndex - 1 + foto.length) % foto.length;
+        setCurrentImageIndex((currentImageIndex - 1 + foto.length) % foto.length);
     };
 
     return (
         <div>
             <button onClick={handlePreviousImage}>Previous</button>
-            <Image src={foto[currentImageIndex]} alt="Current Image" />
+            <Image src={`/${foto[currentImageIndex]}`} alt="Current Image" width="300" height="500" />
             <button onClick={handleNextImage}>Next</button>
         </div>
     );
