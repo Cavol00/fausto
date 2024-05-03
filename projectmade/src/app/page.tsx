6bc673b125959d710e616c97ff104fe44c205b51
Dario D'Alessandro <dario.dalessandro@marconirovereto.it>
2024-05-03 10:34:24 +0200

    fue caroselli

--- projectmade/src/app/page.tsx
+++ projectmade/src/app/page.tsx
@@ -1,15 +1,12 @@
 import Nav from "./Components/Nav/BarNav";
-import dynamic from "next/dynamic";
+import Hom from "./Components/Home/Home";
 import Calendario from "./Components/Calendario/Calendario";
 import Mappa from "./Components/Mappa/Mappa";
-const DynamicHome = dynamic(() => import("./Components/Home/Home"), {
-  ssr: false,
-});
 
 export default function Home() {
   return (
     <main>
-      <Mappa />
-    </main> 
+      <Hom />
+    </main>
   );
 }
